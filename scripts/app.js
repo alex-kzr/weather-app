f5b34eb32f66eb4ea61023ae44a55bfefc310a98
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-16 02:36:35 +1000

    Add ternary operator

--- scripts/app.js
+++ scripts/app.js
@@ -26,18 +26,8 @@ const updateUI = data => {
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute('src', iconSrc);
 
-    let timeSrc = null;
-    if(weather.IsDayTime){
-        timeSrc = 'img/day.svg';
-    }else{
-        timeSrc = 'img/night.svg';
-    }
+    const timeSrc = (weather.IsDayTime) ? 'img/day.svg' : 'img/night.svg';
     time.setAttribute('src', timeSrc);
-
-
-
-
-    console.log(data);
 };
 
 const updateLocation = async locationSearch => {
