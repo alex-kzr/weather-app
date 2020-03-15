db1eba40bd976ae2b78aa35e647d5f590c12c4bb
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-16 00:50:37 +1000

    Add search city API call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,12 @@
-console.log(apiKey);
+const getCity = async (q) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${apiKey}&q=${q}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+getCity('manchester')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
