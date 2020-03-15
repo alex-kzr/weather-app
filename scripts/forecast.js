9cbf7de875154d5a09e874d39e57e8e3f646b719
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-16 01:09:25 +1000

    Add weather API call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,12 +1,23 @@
+// get weather information
+const getWeather = async (locationKey) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationKey}?apikey=${apiKey}`; 
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
+// get city information
 const getCity = async (q) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${apiKey}&q=${q}`;
     const response = await fetch(base + query);
     const data = await response.json();
-
     return data[0];
 };
 
-getCity('manchester')
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+getCity('manchester').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));
